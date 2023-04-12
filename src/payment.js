d6c22811659f05f9fbda156092cfdbdf469ef630
assist <shadow.azray@vk.com>
2023-04-12 20:08:23 +0500

    some files

---
+++ src/payment.js
@@ -0,0 +1,113 @@
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, ListGroup, ListGroupItem, Badge } from 'reactstrap';
+import { useState, useMemo, useCallback } from 'react';
+import { produce } from 'immer';
+
+export function PaymentDialog(args) {
+    
+    const [payment, setPayment] = useState({ influence: { planets: [], tg: 0 }, resources: { planets: [], tg: 0 }, tg: 0 });
+    const [paid, setPaid] = useState({}); //exhausted
+
+    const payPlanet = useCallback((e, planet, type) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if(paid[planet.name] === undefined){
+            setPayment(produce(payment, draft => {
+                draft[type].planets.push(planet);
+            }));
+
+            setPaid(produce(paid, draft => {
+                draft[planet.name] = type;
+            }));
+        }
+    }, [paid, payment]);
+
+    const cancelPlanet = useCallback((pname) => {
+        if(paid[pname] !== undefined){
+            setPayment(produce(payment, draft => {
+                draft[paid[pname]].planets = draft[paid[pname]].planets.filter(p => p.name !== pname);
+            }));
+
+            setPaid(produce(paid, draft => {
+                delete draft[pname];
+            }));
+        }
+    }, [paid, payment]);
+
+    const payTg = useCallback((type) => {
+        setPayment(produce(payment, draft => {
+            draft[type].tg += 1;
+        }));
+    }, [payment]);
+    
+    const tg = useMemo(() => args.race.tg - payment.influence.tg - payment.resources.tg, [payment, args]);
+
+    const acceptable = useMemo(()=>{
+        return Object.keys(args.objective.req).every( tag => {
+            if(tag === 'influence' || tag === 'resources'){
+                const pp = payment[tag].planets.reduce((a,b) => b[tag] + a, 0);
+                return pp + payment[tag].tg >= args.objective.req[tag];
+            }
+            else if(tag === 'tg'){
+                return tg >= args.objective.req[tag];
+            }
+            return false;
+        });
+    }, [payment, args, tg]);
+
+    return (
+        <Modal isOpen={args.isOpen} toggle={args.toggle}>
+        <ModalHeader toggle={args.toggle}>{args.objective.id}</ModalHeader>
+        <ModalBody>
+            {args.objective.title}
+            <div style={{display: 'flex'}}>
+                {args.planets && 
+                <ListGroup style={{width: '50%', margin: '1rem'}}>
+                    {args.planets.map((p, i) => {
+                            let bg = '';
+                            let disabled = false;
+
+                            if (paid[p.name] === 'resources'){ bg = 'beige'; disabled = true}
+                            else if (paid[p.name] === 'influence'){ bg ='aliceblue';  disabled = true}
+                            else if (p.exhausted){ bg = 'silver'; disabled = true}
+
+                            const style={backgroundColor: bg, display: 'flex', justifyContent: 'space-between', cursor: 'default'}
+
+                            return ( 
+                            <ListGroupItem key={i} onClick={(e) => cancelPlanet(p.name)} style={style}>
+                                <div>{p.name}</div>
+                                <div>
+                                    <Button size='sm' style={{color: 'white'}} disabled={disabled} color='warning' onClick={(e)=>payPlanet(e, p, 'resources')}><b>{p.resources}</b></Button>
+                                    <Button size='sm' style={{color: 'white'}} disabled={disabled} color='info' onClick={(e)=>payPlanet(e, p, 'influence')}><b>{p.influence}</b></Button>
+                                </div>
+                            </ListGroupItem>)}
+                    )}
+                </ListGroup>}
+                <div style={{width: '30%', margin: '1rem', display: 'flex', flexDirection: 'column'}}>
+                    {Object.keys(args.objective.req).map((k, i) =>{
+                        
+                        return <div key={i} style={{display: 'flex', justifyContent: 'flex-end'}}>
+                                <Badge color={k ==='influence' ? 'info': ( k === 'resources' ? 'warning' : 'secondary' )} 
+                                        style={{cursor: 'default', fontSize: '1.25rem', marginBottom: '.5rem'}}>
+                                    {payment[k].planets && payment[k].planets.reduce((a,b) => b[k] + a, 0)}
+                                    {!payment[k].planets && tg}
+                                    {payment[k].tg > 0 && '+' + payment[k].tg}
+                                    {' / '}{args.objective.req[k]}
+                                </Badge>
+                                {(k === 'influence' || k === 'resources') && 
+                                <Button disabled={tg < 1} onClick={()=>payTg(k)} size='sm' style={{margin: '0 0 .5rem .5rem'}}>+</Button>}
+                            </div>
+                    }
+                    )}
+                </div>
+            </div>
+        </ModalBody>
+        <ModalFooter>
+            <Button color="primary" disabled = {!acceptable} onClick={(e) => args.toggle(e, payment)}>
+                Confirm
+            </Button>
+        </ModalFooter>
+        </Modal>
+    );
+}
---
+++ src/utils.js
@@ -0,0 +1,83 @@
+export const getPlayerUnits = (tiles, playerID)=> {
+    const units = [];
+  
+    tiles.forEach( t => {
+      if(t.tdata.occupied === playerID){
+        if(t.tdata.fleet){
+          Object.keys(t.tdata.fleet).forEach( k => {
+            if(!units[k]) units[k] = 0;
+            units[k] += t.tdata.fleet[k];
+          });
+        }
+      }
+  
+      if(t.tdata.planets && t.tdata.planets.length){
+        t.tdata.planets.forEach(p => {
+          if(p.occupied === playerID){
+            if(p.units){
+              Object.keys(p.units).forEach( k => {
+                if(!units[k]) units[k] = 0;
+                units[k] += p.units[k];
+              });
+            }
+          }
+        })
+      }
+    });
+    
+    return units;
+  };
+  
+export const getPlayerPlanets = (tiles, playerID)=> {
+    const arr = [];
+  
+    tiles.forEach( t => {
+      if(t.tdata.planets && t.tdata.planets.length){
+  
+          t.tdata.planets.forEach(p => {
+            if(p.occupied === playerID){
+              arr.push({...p, 'systemType': t.tdata.type, 'tid': t.tid});
+            }
+          })
+        
+      }
+    });
+  
+    return arr;
+  };
+  
+export const checkSpend = (G, req, playerID) => {
+  
+    const rkeys = Object.keys(req);
+    const planets = getPlayerPlanets(G.tiles, playerID);
+    const race = G.races[playerID];
+  
+    let influence = 0;
+    let resource = 0;
+    
+    planets.forEach( p => {
+      influence += p.influence;
+      resource += p.resources;
+    });
+  
+    for(var i=0; i < rkeys.length; i++){
+      switch(rkeys[i]){
+        case 'tg':
+          if(race.tg < req[rkeys[i]]) return;
+          break;
+        case 'influence':
+          if(influence + race.tg < req[rkeys[i]]) return;
+          break;
+        case 'resource':
+          if(resource + race.tg < req[rkeys[i]]) return;
+          break;
+        case 'token':
+          if(race.tokens.t + race.tokens.s < req[rkeys[i]]) return;
+          break;
+        default:
+          break;
+      }
+    }
+  
+    return true;
+  }
